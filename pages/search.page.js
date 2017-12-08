08afd1b4f3e5be99e987bc54e370826a23738f8b
Vladislav Korbut <v.korbut8@gmail.com>
2017-12-08 11:39:34 +0300

    Rewrite search test to Page object pattern

--- pages/search.page.js
+++ pages/search.page.js
@@ -1,53 +1,76 @@
 const Page = require('./page');
-const { base } = require('../test/urls');
+const { search } = require('../test/urls');
 
 const getInnerHtml = html => html.replace(/(<([^>]+)>)/ig, "");
 
 class SearchPage extends Page {
 
   open() {
-    super.open(base);
+  super.open(search);
   }
 
   get results() {
     return this.bookingFrame.$('#search-result');
   }
 
-  get bookingFrame() {
-    return super.frame('booking');
+  get resultList() {
+    return this.results.$$('.journey-list');
   }
 
-  set departureCity(city) {
+  get bookingFrame() {
+    if (this._bookingFrame) {
+      return this._bookingFrame;
+    } else {
+      return this._bookingFrame = super.frame('booking');
+    }
+  }
+
+  set city(city) {
     const frame = this.bookingFrame;
-    frame.$('.btn.dropdown-toggle.btn-default[title="Departure city"]').click();
     frame
       .$('.btn-group.bootstrap-select.form-control.open')
       .$('.bs-searchbox')
       .$('input')
       .setValue(city)
-
+    frame
       .$('.btn-group.bootstrap-select.form-control.open')
       .$('.dropdown-menu.inner')
       .$('.active')
       .click();
+  }
+
+  set departureCity(city) {
+    const frame = this.bookingFrame;
+    frame.$('.btn.dropdown-toggle.btn-default[title="Departure city"]').click();
+    this.city = city;
   }
 
   set destinationCity(city) {
     const frame = this.bookingFrame;
-    frame
-      .$('.btn-group.bootstrap-select.form-control.open')
-      .$('.bs-searchbox')
-      .$('input')
-      .setValue(city);
-    frame
-      .$('.btn-group.bootstrap-select.form-control.open')
-      .$('.dropdown-menu.inner')
-      .$('.active')
-      .click();
+    this.city = city;
+  }
+
+  set date(date) {
+    this.bookingFrame.$('#date').setValue(date);
+  }
+
+  get date() {
+    return this.bookingFrame.$('#date').getValue();
+  }
+
+  get formValues() {
+    return [].map.call(
+      this.bookingFrame.$$('.selected.active'),
+      item => getInnerHtml(item.$('span.text').getHTML())
+    );
+  }
+
+  get alert() {
+    return $('.alert.alert-info.text-center').getText();
   }
 
   search() {
-
+    browser.submitForm('#search-form');
   }
 
 }
--- test/search.test.js
+++ test/search.test.js
@@ -15,47 +15,38 @@ describe('Search page', () => {
 
   it('shouldn\'t find anything with empty search query', () => {
     MainPage.search();
-    assert.equal('NoSuchElement', SearchPage.results.type)
+    assert.equal('NoSuchElement', SearchPage.results.type);
   });
 
   it('should be able to filling fields', () => {
     SearchPage.departureCity = searchData.from;
     SearchPage.wait(500);
 
-    SearchPage.departureCity = searchData.to;
+    SearchPage.destinationCity = searchData.to;
     SearchPage.wait(500);
 
-    const dateField = frame.$('#date')
-    dateField.setValue(searchData.wrongDate);
-    const values = [].map.call(frame.$$('.selected.active'), item => getInnerHtml(item.$('span.text').getHTML()));
+    SearchPage.date = searchData.wrongDate;
+    const values = SearchPage.formValues;
 
     assert(~values.indexOf(searchData.from) &&
       ~values.indexOf(searchData.to) &&
-      dateField.getValue() === searchData.wrongDate
+      SearchPage.date === searchData.wrongDate
     );
 
   });
 
   it('shouldn\'t find anything with wrong date and should show alert', () => {
-    browser.submitForm('#search-form');
-    const alertText = $('.alert.alert-info.text-center').getText();
-    assert.equal(alertText, 'According to specified criteria we did not find a trip');
+    SearchPage.search();
+    SearchPage.wait(500);
+    assert.equal(SearchPage.alert, 'According to specified criteria we did not find a trip');
   });
 
   it('should find somethibg with valid date and directions', () => {
-    browser.url(search);
+    SearchPage.wait(500);
 
-    const frame = browser.frame('booking');
+    SearchPage.date = searchData.date;
+    SearchPage.search();
 
-    browser.pause(500)
-
-    const dateField = frame.$('#date')
-    dateField.setValue(searchData.date);
-    const values = [].map.call(frame.$$('.selected.active'), item => getInnerHtml(item.$('span.text').getHTML()));
-    browser.submitForm('#search-form');
-
-    const results = frame.$('#search-result').$$('.journey-list');
-
-    assert(results.length !== 0);
+    assert(SearchPage.resultList.length !== 0);
   });
 });
